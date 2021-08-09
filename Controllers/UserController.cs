7032266648cfe321bf6b0e7e38c24e8e33e05194
Nandan-Wewhare <nandanwewhare25@gmail.com>
2021-08-09 16:38:23 +0530

    completed auth

---
+++ Controllers/UserController.cs
@@ -0,0 +1,55 @@
+﻿using LocalFoodBusinessLayer.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace LocalFoodBusinessLayer.Controllers
+{
+    [RoutePrefix("api/User")]
+    public class UserController : ApiController
+    {
+        [HttpGet]
+        [Route("GetProfile")]
+        public HttpResponseMessage Get([FromUri] int userId)
+        {
+            try
+            {
+                var context = new LocalFoodDBContext();
+                User searchedUser = context.Users.FirstOrDefault(u => u.UserId == userId);
+                return Request.CreateResponse(HttpStatusCode.OK, searchedUser);
+            }
+            catch (Exception ex)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.InternalServerError, $"Please try again{ex.Message}");
+            }
+        }
+
+        [HttpPatch]
+        [Route("UpdateUser")]
+        public HttpResponseMessage Update([FromUri] int userId, [FromBody] UpdateUser user)
+        {
+            try
+            {
+                var context = new LocalFoodDBContext();
+                User searchedUser = context.Users.FirstOrDefault(u => u.UserId == userId);
+                searchedUser.Address = user.Address;
+                searchedUser.City = user.City;
+                searchedUser.Email = user.Email;
+                searchedUser.Gender = user.Gender;
+                searchedUser.Mobile = user.Mobile;
+                searchedUser.Password = user.Password;
+                searchedUser.Pincode = user.Pincode;
+                searchedUser.UserName = user.UserName;
+                context.SaveChanges();
+                return Request.CreateResponse(HttpStatusCode.OK, searchedUser);
+            }
+            catch (Exception ex)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.InternalServerError, $"Please try again{ex.Message}");
+            }
+        }
+    }
+}
---
+++ Migrations/202108091059200_AddedAdminUsers.cs
@@ -0,0 +1,27 @@
+﻿namespace LocalFoodBusinessLayer.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AddedAdminUsers : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.AdminUsers",
+                c => new
+                    {
+                        AdminId = c.Int(nullable: false, identity: true),
+                        Username = c.String(nullable: false),
+                        Password = c.String(nullable: false),
+                    })
+                .PrimaryKey(t => t.AdminId);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.AdminUsers");
+        }
+    }
+}
